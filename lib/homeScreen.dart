61e7d6205312374a946ad8d0f60032027ad1c398
Abood <Aboodwade30@gmail.com>
2023-04-01 03:58:11 +0300

    create body

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,11 +8,71 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.white, Colors.white70, Colors.white],
+          stops: [0.1, 0.8, 0.99],
+        ),
+      ),
       child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 48, 47, 47),
+        backgroundColor: Colors.transparent,
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.transparent,
+          title: Text(
+            'Notes',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+            ),
+          ),
+          // actions: [
+          //   Container(
+          //       decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(10),
+          //         color: Colors.amber,
+          //       ),
+          //       margin: EdgeInsets.all(10),
+          //       padding: EdgeInsets.all(5),
+          //       child: Icon(
+          //         Icons.search,
+          //         color: Colors.black,
+          //       )),
+          //   OutlinedButton(
+          //       style: IconButton.styleFrom(backgroundColor: Colors.red),
+          //       onPressed: () {},
+          //       child: Icon(
+          //         Icons.search,
+          //         color: Colors.black,
+          //       )),
+          // ],
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image.asset(
+                  'images/rafiki.png',
+                  fit: BoxFit.fill,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Create your first note !',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
         ),
       ),
     );
