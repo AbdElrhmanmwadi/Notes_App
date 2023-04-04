ba15aa8906ea5976a5bebba35aa6e8a184b506cb
Abood <Aboodwade30@gmail.com>
2023-04-05 00:51:14 +0300

    show dialog for discard change

--- lib/addnote.dart
+++ lib/addnote.dart
@@ -1,12 +1,22 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
-class Addnote extends StatelessWidget {
+class Addnote extends StatefulWidget {
   const Addnote({Key? key}) : super(key: key);
 
   @override
+  State<Addnote> createState() => _AddnoteState();
+}
+
+class _AddnoteState extends State<Addnote> {
+  @override
   Widget build(BuildContext context) {
+    bool _canPop = false;
+    TextEditingController titleController = TextEditingController();
+    TextEditingController bodyController = TextEditingController();
     return Container(
       decoration: BoxDecoration(
         gradient: const LinearGradient(
@@ -16,103 +26,182 @@ class Addnote extends StatelessWidget {
           stops: [0.1, 0.8, 0.99],
         ),
       ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          title: InkWell(
-            splashFactory: InkSplash.splashFactory,
-            borderRadius: BorderRadius.circular(10),
-            onTap: () {},
-            child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.black12,
-                ),
-                padding: EdgeInsets.all(7),
-                child: Icon(
-                  Icons.arrow_back_ios_new,
-                  color: Colors.black,
+      child: WillPopScope(
+        onWillPop: () async {
+          if (_canPop) {
+            return true;
+          } else {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                actionsAlignment: MainAxisAlignment.spaceEvenly,
+                backgroundColor: Colors.white38,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                title: Center(
+                    child: Icon(
+                  Icons.error,
+                  color: Colors.white,
                 )),
+                content: Text(
+                  "Are your sure you want discard your changes ?",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.normal),
+                ),
+                actions: [
+                  MaterialButton(
+                    color: Colors.white54,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Keep"),
+                  ),
+                  MaterialButton(
+                    textColor: Colors.white,
+                    color: Colors.white38,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    onPressed: () {
+                      setState(() {
+                        _canPop = true;
+                      });
+                      Navigator.of(context).pushNamed('HomeScreen');
+                    },
+                    child: Text("Discard"),
+                  ),
+                ],
+              ),
+            );
+            return false;
+          }
+        },
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: InkWell(
+              splashFactory: InkSplash.splashFactory,
+              borderRadius: BorderRadius.circular(10),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black12,
+                  ),
+                  padding: EdgeInsets.all(7),
+                  child: Icon(
+                    Icons.arrow_back_ios_new,
+                    color: Colors.black,
+                  )),
+            ),
+            actions: [
+              //
+              ButtonBar(
+                children: [
+                  //
+                  InkWell(
+                    splashFactory: InkSplash.splashFactory,
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {},
+                    child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.black12,
+                        ),
+                        padding: EdgeInsets.all(7),
+                        child: Icon(
+                          Icons.save_rounded,
+                          color: Colors.black,
+                        )),
+                  ),
+                  //
+                  InkWell(
+                    splashFactory: InkSplash.splashFactory,
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {},
+                    child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.black12,
+                        ),
+                        padding: EdgeInsets.all(7),
+                        child: Icon(
+                          Icons.archive_rounded,
+                          color: Colors.black,
+                        )),
+                  ),
+                ],
+              )
+            ],
           ),
-          actions: [
-            //
-            ButtonBar(
-              children: [
-                //
-                InkWell(
-                  splashFactory: InkSplash.splashFactory,
-                  borderRadius: BorderRadius.circular(10),
-                  onTap: () {},
-                  child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.black12,
-                      ),
-                      padding: EdgeInsets.all(7),
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.black,
-                      )),
-                ),
-                //
-                InkWell(
-                  splashFactory: InkSplash.splashFactory,
-                  borderRadius: BorderRadius.circular(10),
-                  onTap: () {},
-                  child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.black12,
-                      ),
-                      padding: EdgeInsets.all(7),
-                      child: Icon(
-                        Icons.error_outline_outlined,
-                        color: Colors.black,
-                      )),
-                ),
-              ],
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: TextFormField(
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      hintText: 'Title',
-                      hintStyle: TextStyle(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      controller: titleController,
+                      style: TextStyle(
                         color: Colors.black,
                         fontStyle: FontStyle.italic,
                         fontSize: 25,
                       ),
-                      border: InputBorder.none,
+                      maxLines: null,
+                      maxLength: 100,
+                      buildCounter: (context,
+                              {required currentLength,
+                              required isFocused,
+                              required maxLength}) =>
+                          Text('$currentLength'),
+                      decoration: InputDecoration(
+                        hintText: 'Title',
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 25,
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: TextFormField(
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      hintText: 'Type something...',
-                      hintStyle: TextStyle(
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                      controller: bodyController,
+                      style: TextStyle(
                         color: Colors.black,
                         fontStyle: FontStyle.italic,
                         fontSize: 20,
                       ),
-                      border: InputBorder.none,
+                      maxLines: null,
+                      maxLength: 20,
+                      buildCounter: (context,
+                              {required currentLength,
+                              required isFocused,
+                              required maxLength}) =>
+                          Text('$currentLength'),
+                      decoration: InputDecoration(
+                        hintText: 'Type something...',
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20,
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
