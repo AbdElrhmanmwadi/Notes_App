38dfaa99e0acff5e2e6064d7ecc10e75c9af815b
Abood <Aboodwade30@gmail.com>
2023-04-24 20:47:00 +0300

    Add page one and page two

---
+++ lib/pageone.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'package:note/DeleteBottomSheet.dart';
+import 'package:note/SearchTextFormField.dart';
+import 'package:note/bottomShettForDeleteOrShare.dart';
+import 'package:note/homeScreen.dart';
+import 'package:note/sql/SqlDb.dart';
+import 'package:note/viewNote.dart';
+
+import 'cardNote.dart';
+import 'controller/sqlConrtoller.dart';
+
+class pageOne extends StatelessWidget {
+  const pageOne({
+    super.key,
+    required this.controller,
+    required this.sqlDb,
+  });
+
+  final SqlController controller;
+  final SqlDb sqlDb;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            SearchTextFormField(
+              hintText: 'Search notes',
+              icon: Icons.search,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Expanded(
+                child: FutureBuilder(
+              future: controller.readData('notes'),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return StaggeredGridView.countBuilder(
+                      staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                      padding: EdgeInsets.zero,
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) =>
+                          new ClipRRect(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0)),
+                            child: GestureDetector(
+                              onTap: () {
+                                Get.to(
+                                  ViewNote(
+                                    title: '${snapshot.data[index]['title']}',
+                                    body: '${snapshot.data[index]['note']}',
+                                    id: snapshot.data[index]['id'],
+                                  ),
+                                );
+                              },
+                              onLongPress: () {
+                                Get.bottomSheet(
+                                    barrierColor: Colors.transparent,
+                                    bottomShettForDeleteOrShare(
+                                      sqlDb: sqlDb,
+                                      Widgett: deleteBottomSeet(
+                                        sqlDb: sqlDb,
+                                        function: () async {
+                                          await sqlDb.delete('notes',
+                                              'id=${snapshot.data[index]['id']}');
+                                          Get.offAll(HomeScreen(initIndex: 0,));
+                                        },
+                                      ),
+                                    ));
+                              },
+                              child: CardNote(
+                                  title: '${snapshot.data[index]['title']}',
+                                  Body: '${snapshot.data[index]['note']}',
+                                  date: '${snapshot.data[index]['date']}'),
+                            ),
+                          ));
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation(Colors.amber),
+                    ),
+                  );
+                }
+              },
+            )),
+          ],
+        ));
+  }
+}
+
+
---
+++ lib/pagetwo.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:note/DeleteBottomSheet.dart';
+import 'package:note/bottomShettForDeleteOrShare.dart';
+import 'package:note/bottomShettTask.dart';
+import 'package:note/controller/sqlConrtoller.dart';
+import 'package:note/dimensions.dart';
+import 'package:note/homeScreen.dart';
+import 'package:note/sql/SqlDb.dart';
+import 'package:note/styles.dart';
+
+class pageTwo extends StatelessWidget {
+  const pageTwo({
+    super.key,
+    required this.controller,
+    required this.sqlDb,
+   
+    required this.taskController,
+  });
+
+  final SqlController controller;
+  final SqlDb sqlDb;
+ 
+  final TextEditingController taskController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height,
+      child: FutureBuilder(
+        future: controller.readData('tasks'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) => Card(
+                      child: ListTile(
+                        onLongPress: () {
+                          Get.bottomSheet(
+                              barrierColor: Colors.transparent,
+                              bottomShettForDeleteOrShare(
+                                sqlDb: sqlDb,
+                                Widgett: deleteBottomSeet(
+                                  sqlDb: sqlDb,
+                                  function: () async {
+                                    await sqlDb.delete('tasks',
+                                        'id=${snapshot.data[index]['id']}');
+
+                                    Get.offAll(HomeScreen(
+                                      initIndex: 1,
+                                    ));
+                                  },
+                                ),
+                              ));
+                        },
+                        onTap: () {
+                          Get.bottomSheet(
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: bottomShettTask(
+                                taskController: taskController,
+                                sqlDb: sqlDb,
+                                initValue: snapshot.data[index]['task'],
+                                onPressed: () async {
+                                  var response = await sqlDb.update(
+                                      'tasks',
+                                      {
+                                        'task': "${taskController.text}",
+                                      },
+                                      'id=${snapshot.data[index]['id']}');
+                                  print(response);
+                                  if (response > 0) {
+                                    Get.back();
+                                  }
+                                },
+                              ),
+                            ),
+                          );
+                        },
+                        leading: Checkbox(value: true, onChanged: (c) {}),
+                        title: Text(
+                          '${snapshot.data[index]['task']}',
+                          style: robotoMedium.copyWith(
+                              fontSize: Dimensions.fontSizeLarge),
+                        ),
+                      ),
+                    ));
+          } else {
+            return Center(
+              child: Text('No task '),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
