8fb713c93b889f8c9e2ac5a5c62208cc8ef59dc2
test1 <Aboodwade30@gmail.com>
2023-08-07 10:43:29 +0300

    remove  CheckboxListTile

--- lib/src/features/home/widget/pagetwo.dart
+++ lib/src/features/home/widget/pagetwo.dart
@@ -35,7 +35,7 @@ class pageTwo extends StatefulWidget {
 }
 
 class _pageTwoState extends State<pageTwo> {
-  bool isExpandedd = true;
+  bool isExpandedd = false;
   List<TaskModel> completedList = [];
   List<TaskModel> incompleteList = [];
 
@@ -52,7 +52,6 @@ class _pageTwoState extends State<pageTwo> {
         child: Container(
             padding: EdgeInsets.symmetric(horizontal: 15),
             child: Column(children: [
-              Text('complet'),
               BlocBuilder<TaskBloc, TaskState>(
                 builder: (context, state) {
                   if (state is LoadedTaskState) {
@@ -138,7 +137,7 @@ class _taskwidgetState extends State<taskwidget> {
         shape: OutlineInputBorder(
             borderRadius: BorderRadius.circular(15),
             borderSide: BorderSide.none),
-        child: GestureDetector(
+        child: InkWell(
           onLongPress: () {
             showModalBottomSheet(
               context: context,
@@ -188,13 +187,14 @@ class _taskwidgetState extends State<taskwidget> {
                     sqlDb: widget.sqlDb,
                     initValue: widget.snapshot[index].task,
                     onPressed: () async {
-                      BlocProvider.of<TaskBloc>(context).add(UpdateTaskEvent(
-                        'tasks',
-                        'id=${widget.snapshot[index].id}',
-                        {
-                          'task': "${widget.taskController.text}",
-                        },
-                      ));
+                      context.read<TaskBloc>().add(UpdateTaskEvent(
+                            'tasks',
+                            'id=${widget.snapshot[index].id}',
+                            {
+                              'task': "${widget.taskController.text}",
+                            },
+                          ));
+
                       Navigator.pop(context);
                     },
                   ),
@@ -204,33 +204,41 @@ class _taskwidgetState extends State<taskwidget> {
           },
           child: InputDecorator(
             decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 5),
+                contentPadding: EdgeInsets.symmetric(vertical: 10),
                 fillColor: Colors.white,
                 filled: true,
                 border: OutlineInputBorder(
                     borderSide: BorderSide.none,
                     borderRadius: BorderRadius.circular(15))),
-            child: CheckboxListTile(
-              selected: widget.snapshot[index].isComplete == 0 ? true : false,
-              checkColor: Colors.white,
-              activeColor: Colors.amber,
-              title: Text('${widget.snapshot[index].task}'),
-              value: widget.snapshot[index].isComplete == 0 ? true : false,
-              onChanged: (bool? newValue) {
-                int val = newValue == true ? 0 : 1;
-                setState(() {
-                  widget.snapshot[index].isComplete = val;
-                });
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Checkbox(
+                    checkColor: Colors.white,
+                    activeColor: Colors.amber,
+                    value:
+                        widget.snapshot[index].isComplete == 0 ? true : false,
+                    onChanged: (_) {
+                      int val = widget.snapshot[index].isComplete == 0 ? 1 : 0;
+                      setState(() {
+                        widget.snapshot[index].isComplete = val;
+                      });
 
-                Future.delayed(Duration(seconds: 1), () {
-                  BlocProvider.of<TaskBloc>(context).add(UpdateTaskEvent(
-                    'tasks',
-                    'id=${widget.snapshot[index].id}',
-                    {'isComplete': val},
-                  ));
-                });
-              },
-              controlAffinity: ListTileControlAffinity.leading,
+                      Future.delayed(Duration(seconds: 1), () {
+                        BlocProvider.of<TaskBloc>(context).add(UpdateTaskEvent(
+                          'tasks',
+                          'id=${widget.snapshot[index].id}',
+                          {'isComplete': val},
+                        ));
+                      });
+                    }, // Keep onChanged as an empty function
+                  ),
+                ),
+                Expanded(
+                  child: Text('${widget.snapshot[index].task}'),
+                ),
+              ],
             ),
           ),
         ),
@@ -238,109 +246,3 @@ class _taskwidgetState extends State<taskwidget> {
     );
   }
 }
-
-// SingleChildScrollView(
-//       child: Container(
-//         padding: EdgeInsets.symmetric(horizontal: 15),
-//         child: Column(
-//           children: [
-//             Text('complet'),
-//             BlocBuilder<TaskBloc, TaskState>(builder: (context, state) {
-//               if (state is LoadedTaskState) {
-//                 final completedList =
-//                     state.tasks.where((note) => note.isComplete == 1).toList();
-//                 final incompleteList =
-//                     state.tasks.where((note) => note.isComplete == 0).toList();
-
-//                 return Column(
-//                   children: [
-//                     // ListTile(
-//                     //   title: Text('Completed ${completedList.length}'),
-//                     //   leading: false
-//                     //       ? Icon(Icons.arrow_drop_up)
-//                     //       : Icon(Icons.arrow_drop_down),
-//                     //   onTap: () {
-//                     //     BlocProvider.of<TaskBloc>(context).toggleExpansion();
-//                     //   },
-//                     // ),
-//                     // if (state is Expando)
-//                     //   taskwidget(
-//                     //     sqlDb: widget.sqlDb,
-//                     //     taskController: widget.taskController,
-//                     //     snapshot: completedList,
-//                     //   ),
-//                     // taskwidget(
-//                     //   sqlDb: widget.sqlDb,
-//                     //   taskController: widget.taskController,
-//                     //   snapshot: incompleteList,
-//                     // ),
-//                   ],
-//                 );
-//               } else if (state is LoadingTaskState) {
-//                 return CircularProgresIndicator();
-//               } else if (state is EmptyTaskState) {
-//                 return Text('Error');
-//               }
-//               return Container();
-//             }),
-//           ],
-//         ),
-//       ),
-//     );
-
-
-
-//  GetBuilder<SqlController>(
-//                   init: SqlController(),
-//                   builder: (context) => FutureBuilder(
-//                         future: controller.readData('tasks', 'isComplete=1'),
-//                         builder: (context, snapshot) {
-//                           if (snapshot.hasData) {
-//                             return taskwidget(
-//                               sqlDb: sqlDb,
-//                               taskController: taskController,
-//                               snapshot: snapshot,
-//                             );
-//                           } else {
-//                             return CircularProgresIndicator();
-//                           }
-//                         },
-//                       )),
-//               GetBuilder<SqlController>(
-//                   init: SqlController(),
-//                   builder: (context) => FutureBuilder(
-//                         future: controller.readData('tasks', 'isComplete=0'),
-//                         builder: (context, snapshot) {
-//                           if (snapshot.hasData) {
-//                             return Column(
-//                               crossAxisAlignment: CrossAxisAlignment.start,
-//                               children: [
-//                                 Column(
-//                                   children: [
-//                                     ListTile(
-                                     
-//                                       title: Text(
-//                                         'Completed ${snapshot.data.length} ',
-//                                       ),
-//                                       leading: isExpanded
-//                                           ? Icon(Icons.arrow_drop_up)
-//                                           : Icon(Icons.arrow_drop_down),
-//                                       onTap: () {
-//                                         isExpanded = !isExpanded;
-//                                       },
-//                                     ),
-//                                     if (isExpanded)
-//                                       taskwidget(
-//                                         sqlDb: sqlDb,
-//                                         taskController: taskController,
-//                                         snapshot: snapshot,
-//                                       ),
-//                                   ],
-//                                 ),
-//                               ],
-//                             );
-//                           } else {
-//                             return CircularProgresIndicator();
-//                           }
-//                         },
-//                       )),
