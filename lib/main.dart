0b358e0ff9e9968e6d367d546d978bee14417f26
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2026-06-29 16:15:45 +0300

    feat: Initialize AppPaths in main function for improved app setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/core/app_paths.dart';
 import 'src/core/notifications/notification_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await AppPaths.init();
   await NotificationService.instance.init();
   // Fire-and-forget: don't block first paint on the permission dialog.
   NotificationService.instance.requestPermissions();
